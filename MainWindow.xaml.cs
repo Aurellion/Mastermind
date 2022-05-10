302dfab330523cfde6f37f196403dfa9df99afd3
Johannes B <johannes.bock@live.de>
2022-05-10 13:08:27 +0200

    Farben und Beginn Ratecode

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace Mastermind
     {
         List<Ellipse> ellipses = new List<Ellipse>();
         List<TextBox> textBoxes = new List<TextBox>();
+        int[] code = new int[4];//0=Schwarz, 1=Blau, 2=Rot, 3=Grün
+        Random rnd = new Random();
         int Runde = 0;
         public MainWindow()
         {
@@ -52,6 +54,10 @@ namespace Mastermind
         private void Btn_Start_Click(object sender, RoutedEventArgs e)
         {
             NeueZeile();
+            for(int i=0; i<4; i++)
+            {
+                code[i] = rnd.Next(0,4);
+            }
             Btn_Start.IsEnabled = false;
             Btn_Spiel.IsEnabled = true;
         }
@@ -68,6 +74,7 @@ namespace Mastermind
             Spielfeld.RowDefinitions.Add(rowDefinition);
 
             //Ellipsen
+            ellipses.Clear();
             for (int i = 0; i < 4; i++)
             {
                 //Ellipse definieren
@@ -83,6 +90,21 @@ namespace Mastermind
                 menuItemBlack.Header = "Schwarz";
                 menuItemBlack.Click += new RoutedEventHandler(EllipseSchwarzFärben);
                 contextMenu.Items.Add(menuItemBlack);
+
+                MenuItem menuItemBlue = new MenuItem();
+                menuItemBlue.Header = "Blau";
+                menuItemBlue.Click += new RoutedEventHandler(EllipseBlauFärben);
+                contextMenu.Items.Add(menuItemBlue);
+
+                MenuItem menuItemRed = new MenuItem();
+                menuItemRed.Header = "Rot";
+                menuItemRed.Click += new RoutedEventHandler(EllipseRotFärben);
+                contextMenu.Items.Add(menuItemRed);
+
+                MenuItem menuItemGreen = new MenuItem();
+                menuItemGreen.Header = "Grün";
+                menuItemGreen.Click += new RoutedEventHandler(EllipseGrünFärben);
+                contextMenu.Items.Add(menuItemGreen);
 
 
                 ellipse.ContextMenu = contextMenu;
@@ -119,6 +141,75 @@ namespace Mastermind
             ContextMenu cm = mi.Parent as ContextMenu;
             Ellipse el = cm.PlacementTarget as Ellipse;
             el.Fill = Brushes.Black;
+        }
+
+        private void EllipseBlauFärben(object sender, RoutedEventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;            
+            Ellipse el = ((ContextMenu)mi.Parent).PlacementTarget as Ellipse;
+            el.Fill = Brushes.Blue;
+        }
+
+        private void EllipseRotFärben(object sender, RoutedEventArgs e)
+        {           
+            Ellipse el = ((ContextMenu)((MenuItem)e.Source).Parent).PlacementTarget as Ellipse;
+            el.Fill = Brushes.Red;
+        }
+
+        private void EllipseGrünFärben(object sender, RoutedEventArgs e)
+        {
+            ((Ellipse)((ContextMenu)((MenuItem)e.Source).Parent).PlacementTarget).Fill = Brushes.Green;
+        }
+
+        private void Btn_Spiel_Click(object sender, RoutedEventArgs e)
+        {
+            int[] geraten = new int[4];
+            for (int i = 0; i < geraten.Length; i++)
+            {
+                geraten[i] = FarbeZuZahl(ellipses[i].Fill);
+            }
+            if (geraten.Contains(-1))
+            {
+                MessageBox.Show("Bitte Farben für alle Kreise wählen.", "Fehler!");
+            }
+            else Raten(geraten);
+        }
+
+        private void Raten(int[] geraten)
+        {
+            string[] ausgabe = new string[4];
+            for (int i = 0; i < geraten.Length; i++)
+            {
+                if (geraten[i] == code[i]) ausgabe[i] = "X";
+                else if (code.Contains(geraten[i])) ausgabe[i] = "O";
+                else ausgabe[i] = " ";
+            }
+            Mischen(ausgabe);
+
+        }
+
+        public void Mischen(string[] stringArray) 
+        {
+            //stringArray[2] = "5";//automatisch -> ausgabe[2]="5"
+            int n = stringArray.Length;
+            while (n > 1)
+            {
+                int k = rnd.Next(n--);//n-- -> n = n-1;
+                //string temp = stringArray[n];
+                //stringArray[n] = stringArray[k];
+                //stringArray[k] = temp;
+                (stringArray[n], stringArray[k]) = (stringArray[k], stringArray[n]);
+            }
+        }
+
+        public int FarbeZuZahl(Brush color)
+        {
+            int zahl=-1;
+            if (color == Brushes.Black) zahl = 0;
+            else if (color == Brushes.Blue) zahl = 1;
+            else if (color == Brushes.Red) zahl = 2;
+            else if (color == Brushes.Green)zahl = 3;
+            return zahl;
         }
     }
 }
